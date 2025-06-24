3ed6e3bae4d099df12b89ad9d207813087245509
Kunal Kumavat <kumavatkunal1@gmail.com>
2025-06-24 23:06:10 +0530

    Frontend conversation structure

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,18 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="../templates")
 
 
 @app.route("/health")
 def health():
     return jsonify({"Status": "OK"})
 
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
